6101e991f0f1d4f191dd6d593f394d3cc4b88386
Yaseen Hasan <yabuzeina02@gmail.com>
2026-01-04 23:55:29 +0200

    2

--- src/main/java/com/grabx/app/grabx/GrabXApp.java
+++ src/main/java/com/grabx/app/grabx/GrabXApp.java
@@ -21,6 +21,7 @@ public class GrabXApp extends Application {
 //                java.util.Objects.requireNonNull(GrabXApp.class.getResource("styles/table.css")).toExternalForm(),
 //                java.util.Objects.requireNonNull(GrabXApp.class.getResource("styles/sidebar.css")).toExternalForm()
 //        );
+
         stage.setTitle("GrabX");
         stage.setMinWidth(900);
         stage.setMinHeight(500);
